34f3469f8dc543efc19ab86d5713cfa4d07a225d
jfkunz <johannes.f.kunz@gmail.com>
2021-03-22 13:27:01 +0100

    added https

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var jkRouter = require('./routes/indexjk')
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/letter', letterRouter);
